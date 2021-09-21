a62ff3e729eea8481ad89e9e520de4da30368085
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-09-21 10:00:25 +0100

    Add a remove book function

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const booksList = document.querySelector("#book-list");
 let booklist = [];
 let bookHtml = "";
-
-
 if (JSON.parse(localStorage.getItem('books')) == null) {
-
     const bookObj = { allbook: booklist };
     localStorage.setItem('books', JSON.stringify(bookObj));
 } else {
@@ -12,15 +9,14 @@ if (JSON.parse(localStorage.getItem('books')) == null) {
 
     booklist.forEach((item, index) => {
         bookHtml += `
-    <p>${item.title}</p>
-    <p>${item.author}</p>
-    <button type="button" class="remove-btn" id="${index}">Remove</button>
-    <hr>`;
+<p>${item.title}</p>
+<p>${item.author}</p>
+<button type="button" class="remove-btn" id="${index}">Remove</button>
+<hr>`;
     })
     booksList.innerHTML = bookHtml;
 
 }
-
 
 const bookForm = document.querySelector("#book-form");
 const bookTitle = document.querySelector("#title");
@@ -32,7 +28,6 @@ bookForm.addEventListener('submit', () => {
         author: bookAuthor.value
     };
 
-
     let obj = JSON.parse(localStorage.getItem('books'));
 
     obj.allbook.push(newBook);
@@ -43,13 +38,14 @@ bookForm.addEventListener('submit', () => {
 })
 
 
-remove = () => {
-    document.querySelectorAll('.remove-btn').forEach((item, bookIndex) => {
-      item.addEventListener('click', () => {
-        const newBooksArray = this.obj.filter((booklist, index) => bookIndex !== index);
-        console.log(index.id);
-        localStorage.setItem('books', JSON.stringify(newBooksArray));// set the new book to the local storage
-        window.location.reload();// reload the page
-      });
-    });
-  }
+const removeBtn = document.querySelectorAll(".remove-btn");
+
+removeBtn.forEach((item) => item.addEventListener('click', () => {
+    let removeId = parseInt(item.id);
+    let obj = JSON.parse(localStorage.getItem('books'));
+    booklist = obj.allbook;
+    booklist = booklist.filter((element, index) => index !== removeId);
+    obj.allbook = booklist;
+    localStorage.setItem("books", JSON.stringify(obj));
+    window.location.reload()
+}));
