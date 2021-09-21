32325a3e657f9343d3c9fd63b8f9714a9d808bdb
darrenodi <tantheta85@gmail.com>
2021-09-21 10:25:56 +0100

    Fix merge errors

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const booksList = document.querySelector("#book-list");
 let booklist = [];
 let bookHtml = "";
-
-
 if (JSON.parse(localStorage.getItem('books')) == null) {
-
     const bookObj = { allbook: booklist };
     localStorage.setItem('books', JSON.stringify(bookObj));
 } else {
@@ -12,16 +9,14 @@ if (JSON.parse(localStorage.getItem('books')) == null) {
 
     booklist.forEach((item, index) => {
         bookHtml += `
-    <p>${item.title}</p>
-    <p>${item.author}</p>
-    <button type="button" class="remove-btn" id="${index}">Remove</button>
-    <hr>`;
-    console.log(index);
+<p>${item.title}</p>
+<p>${item.author}</p>
+<button type="button" class="remove-btn" id="${index}">Remove</button>
+<hr>`;
     })
     booksList.innerHTML = bookHtml;
 
 }
-
 
 const bookForm = document.querySelector("#book-form");
 const bookTitle = document.querySelector("#title");
@@ -33,7 +28,6 @@ bookForm.addEventListener('submit', () => {
         author: bookAuthor.value
     };
 
-
     let obj = JSON.parse(localStorage.getItem('books'));
 
     obj.allbook.push(newBook);
@@ -43,39 +37,14 @@ bookForm.addEventListener('submit', () => {
     localStorage.setItem("books", JSON.stringify(obj));
 })
 
-const removeBtn = document.querySelector(".remove-btn");
-// removeBtn.addEventListener('click',() => {
-//   console.log(this.id);
-//   console.log("nigga");
-//   booklist = JSON.parse(localStorage.getItem('books')).allbook;
-//   booklist.forEach((item, index) => {
-//     if (index === removeBtnId) {
-//       window.localStorage.removeItem(item);
-//     }
-// });
-// });
+const removeBtn = document.querySelectorAll(".remove-btn");
 
-removeBtn.addEventListener('click',removeID);
-function removeID(){
-  const removeBtnId = this.id;
-  booklist = JSON.parse(localStorage.getItem('books')).allbook;
-  booklist.forEach((item, index) => {
-    console.log(index.toString() + 5);
-    console.log(removeBtnId + 5);
-    if (index.toString() === removeBtnId) {
-      console.log(item);
-      localStorage.removeItem(item);
-    }
-});
-}
-
-// remove = () => {
-//     document.querySelectorAll('.remove-btn').forEach((item, bookIndex) => {
-//       item.addEventListener('click', () => {
-//         const newBooksArray = this.obj.filter((booklist, index) => bookIndex !== index);
-//         console.log(index);
-//         localStorage.setItem('books', JSON.stringify(newBooksArray));// set the new book to the local storage
-//         window.location.reload();// reload the page
-//       });
-//     });
-//   }
+removeBtn.forEach((item) => item.addEventListener('click', () => {
+    let removeBookId = parseInt(item.id);
+    let obj = JSON.parse(localStorage.getItem('books'));
+    booklist = obj.allbook;
+    booklist = booklist.filter((element, index) => index !== removeBookId);
+    obj.allbook = booklist;
+    localStorage.setItem("books", JSON.stringify(obj));
+    window.location.reload()
+}));
