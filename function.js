c7b1155269aab18f7db5bd382326970fe027494e
basnetlaxmi <ddsanzel@gmail.com>
2021-09-23 23:36:22 +0545

    Add function.js file to display on click menu

---
+++ function.js
@@ -0,0 +1,26 @@
+const section1 = document.querySelector('.container');
+const section2 = document.querySelector('.formSection');
+const section3 = document.querySelector('.information');
+const listsection = document.querySelector('.list');
+const add = document.querySelector('.new');
+const contact = document.querySelector('.contact');
+
+function fun1() {
+  section1.style.display = 'flex';
+  section2.style.display = 'none';
+  section3.style.display = 'none';
+}
+function fun2() {
+  section1.style.display = 'none';
+  section2.style.display = 'block';
+  section3.style.display = 'none';
+}
+function fun3() {
+  section1.style.display = 'none';
+  section2.style.display = 'none';
+  section3.style.display = 'flex';
+}
+
+listsection.addEventListener('click', fun1);
+add.addEventListener('click', fun2);
+contact.addEventListener('click', fun3);
