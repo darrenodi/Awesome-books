b92346bc6023e419fdabbcf9705cb272a93fd67e
basnetlaxmi <ddsanzel@gmail.com>
2021-09-24 12:13:48 +0545

    Remove js file

--- select-link.js
+++
@@ -1,8 +0,0 @@
-function toggleDisplay(element) {
-  console.log(element.id);
-}
-const optionHi = document.querySelectorAll('.menus');
-
-optionHi.forEach((item) => item.addEventListener('click', () => {
-  toggleDisplay(item);
-}));
